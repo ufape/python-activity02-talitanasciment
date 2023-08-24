6c6b90e112387b153ea8b97a540f79c00ff431b0
talitanasciment <142513164+talitanasciment@users.noreply.github.com>
2023-08-24 15:43:14 -0300

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -36,8 +36,18 @@ Fora de intervalo.
 
 
 def main():
-    pass  # your code here
+ 
+    valor = float(input("Digite o valor: "))
 
-
+    if valor >= 0 and valor <= 25:
+        print("Intervalo [0, 25]")
+    elif valor > 25 and valor <= 50:
+        print("Intervalo (25, 50]")
+    elif valor > 50 and valor <= 75:
+        print("Intervalo (50, 75]")
+    elif valor > 75 and valor <= 100:
+        print("Intervalo (75, 100]")
+    else:
+        print("Fora de intervalo.")
 if __name__ == '__main__':
     main()
